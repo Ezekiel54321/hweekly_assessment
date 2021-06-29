637b16037749f28c82157bdc86253030071c400e
Ezekiel12345 <you@example.com>
2021-06-29 17:57:02 +0100

    Assesssment 4 new

--- app/src/main/java/com/ezekiel/myapplication/MainActivity.kt
+++ app/src/main/java/com/ezekiel/myapplication/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     var Username: String = "Admin"
@@ -18,18 +19,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-                login = findViewById(R.id.loginButton)
 
-        login.setOnClickListener{
-            val Username = UsernameInput.text.toString()
-            val Password = PasswordInput.text.toString()
-            if(Username.text.toString().equals()("Admin") && Password=="Admin"){
-               val intent: Intent = Intent( this, MenuActivity2::class.java)
-                }
-            else{
-                println("Username and Password is incorrect")
-            }
+        login = findViewById(R.id.loginButton)
+        UsernameInput = findViewById(R.id.usernameET)
+        PasswordInput = findViewById(R.id.PasswordET)
 
+        login.setOnClickListener {
+            var password = UsernameInput.text.toString()
+            var username = PasswordInput.text.toString()
+            val intent: Intent = Intent(this@MainActivity, MenuActivity2::class.java);
+            startActivity(intent)
+
+        }
+        if (UsernameInput.text.toString().equals("Admin") && PasswordInput.text.toString()
+                .equals("Admin")
+        ) {
+        } else {
+            println("Username and Password is incorrect")
         }
     }
 }
+
+
+
--- app/src/main/java/com/ezekiel/myapplication/MenuActivity2.kt
+++ app/src/main/java/com/ezekiel/myapplication/MenuActivity2.kt
@@ -1,11 +1,16 @@
 package com.ezekiel.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MenuActivity2 : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu2)
+
+        }
     }
-}
